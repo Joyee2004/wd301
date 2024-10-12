7a5ccb62ac55b3f620c12e55bd0fe3c1a1ec6056
Joyee2004 <joyee6biswas@gmail.com>
2024-10-12 16:31:03 +0530

    added conditional rendering of completeddate and dueDate

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -1,21 +1,26 @@
-
 import React from 'react';
 import './TaskCard.css';
+
 interface TaskCardProps {
   card: string;
   title: string;
   dueDate?: Date;
-  completedAtTime?: Date;
+  completedAtDate?: Date;
   assigneeName: string;
 }
-const TaskCard: React.FC<TaskCardProps> = ({ card, title, dueDate, completedAtTime, assigneeName }) => {
+
+const TaskCard: React.FC<TaskCardProps> = ({ card, title, dueDate, completedAtDate, assigneeName }) => {
   return (
     <div className={`TaskItem ${card === 'pending' ? 'pending' : 'completed'}`}>
       <h3 className="title">{title}</h3>
-      {dueDate && <p className="status">Due: {dueDate.toLocaleDateString("en-CA")}</p>}
-      {completedAtTime && <p className="status">Completed: {completedAtTime.toLocaleDateString("en-CA")}</p>}
+     
+      {completedAtDate && <p className="status">Completed: {completedAtDate.toLocaleDateString("en-CA")}</p>}
+      {!completedAtDate && dueDate && (
+        <p>Due on: {dueDate.toLocaleDateString("en-CA")}</p>
+      )}
       <p className="assignee">Assignee: {assigneeName}</p>
     </div>
   );
 };
+
 export default TaskCard;
